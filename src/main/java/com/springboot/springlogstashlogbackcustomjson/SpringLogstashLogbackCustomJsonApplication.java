1b8e7bfef9f33ef50ae8ab1d84e9dd9c35a57b65
Ramprakash <ram7prksh@yahoo.co.in>
2023-07-22 06:45:01 +0530

    Initial commit

---
+++ src/main/java/com/springboot/springlogstashlogbackcustomjson/SpringLogstashLogbackCustomJsonApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.springlogstashlogbackcustomjson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLogstashLogbackCustomJsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLogstashLogbackCustomJsonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/springlogstashlogbackcustomjson/SpringLogstashLogbackCustomJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.springlogstashlogbackcustomjson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLogstashLogbackCustomJsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
